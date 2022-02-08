915936d588260d6d75bdc1ae14e69c4b66e6ff6b
Tufahel <tufahel96@gmail.com>
2022-02-08 16:53:49 +0600

    required changes in js file

--- script.js
+++ script.js
@@ -1,47 +1,95 @@
-const title = document.getElementById('title');
-const author = document.getElementById('author');
-const showBooks = document.getElementById('show-books');
-const submitBtn = document.getElementById('addBtn');
-let books = [];
+(function () {
 
-function display() {
-  document.querySelector('#show-books').innerHTML = '';
-  books.forEach((book) => {
-    showBooks.innerHTML += `
-        <h3>${book.title}</h3>
-        <h3>${book.author}</h3>
-        <button id=${book.id} onClick=removeBook(id) type="submit">Remove</button>
-        <hr>
-        `;
-  });
-  title.value = '';
-  author.value = '';
-}
+  var lastId = 0;
+  var showList = document.getElementById("show_list");
+  var btnSave = document.getElementById("save");
+  var removeData;
+  var dataList;
 
-function addNewBook() {
-  if (title.value.length > 0 && author.value.length > 0) {
-    books.push({
-      title: title.value,
-      author: author.value,
-      id: books.length,
-    });
-    display();
-    addLocalStorage();
+  function init() {
+      if (!!(window.localStorage.getItem('dataList'))) {
+          dataList = JSON.parse(window.localStorage.getItem('dataList'));
+      }
+      else {
+          dataList = [];
+      }
+      btnSave.addEventListener('click', saveData);
+      showData();
   }
-}
-function removeBook(id) {
-  books = books.filter((e) => e.id != id);
-  display();
-  addLocalStorage();
-}
 
-submitBtn.addEventListener('click', addNewBook);
-function addLocalStorage() {
-  const addStorage = JSON.stringify(books);
-  localStorage.setItem('form', addStorage);
-}
-if (localStorage.getItem('form') != null) {
-  const getForm = localStorage.getItem('form');
-  books = JSON.parse(getForm);
-  display();
-}
+  function showData() {
+      if (!!dataList.length) {
+          getLastTaskId();
+          for (var item in dataList) {
+              var data = dataList[item];
+              addDataToList(data);
+          }
+          syncEvents();
+      }
+  }
+
+  function saveData() {
+      var data = {
+          dataId: lastId,
+          dataTitle: document.getElementById("title").value,
+          dataAuthor: document.getElementById("author").value
+      }
+      dataList.push(data);
+      syncTask();
+      addDataToList(data);
+      syncEvents();
+      lastId++;
+  }
+
+  function addDataToList(data) {
+      var removeData = document.createElement('span');
+      var element = document.createElement('li');
+
+      removeData.innerHTML = `<button>Remove</button>`;
+      removeData.className = "remove_data";
+      removeData.setAttribute("title", "Remove");
+
+      element.appendChild(removeData);
+      element.setAttribute("id", data.dataId);
+      element.innerHTML += ' ' + data.dataTitle + ' ' + data.dataAuthor;
+      showList.appendChild(element);
+  }
+
+  function removeDta(event) {
+      var dataToRemove = event.currentTarget.parentNode;
+      console.log('data to remove', dataToRemove)
+      var dataId = dataToRemove.id;
+      console.log('data to remove id', dataToRemove.id)
+      showList.removeChild(dataToRemove);
+      dataList.forEach(function (value, i) {
+          if (value.dataId == dataId) {
+              dataList.splice(i, 1);
+          }
+      })
+      syncTask();
+  }
+
+  function syncTask() {
+
+      window.localStorage.setItem('dataList', JSON.stringify(dataList));
+      dataList = JSON.parse(window.localStorage.getItem('dataList'));
+  }
+
+  function getLastTaskId() {
+      var lastTask = dataList[dataList.length - 1];
+      lastId = lastTask.dataId + 1;
+  }
+
+  function syncEvents() {
+
+      removeData = document.getElementsByClassName("remove_data");
+      if (!!removeData.length) {
+          for (var i = 0; i < removeData.length; i++) {
+              removeData[i].addEventListener('click', removeDta);
+          }
+      }
+  }
+
+  init();
+
+})();
