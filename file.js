218217e15bf2f3c5e87450df969f884bb1467f80
Lynette <lynette@Caryns-MacBook-Air.local>
2022-02-09 12:52:27 +0300

    Fix linter errors

--- file.js
+++ file.js
@@ -8,51 +8,52 @@ class Book {
   }
 }
 
-class bookList {
-    list = []; 
+class BookList {
+    list = [];
 
     addBook() {
-        const title = document.querySelector('#title').value;
-        const author = document.querySelector('#author').value;
-        if (title !== '' && author !== '') {
-            const book = new Book(title, author);
-            this.list.push(book);
-            localStorage.setItem('Books', JSON.stringify(this.list));
-            this.list = JSON.parse(localStorage.getItem('Books'));
-            this.render();
-        }
-        document.querySelector('#title').value = '';
-        document.querySelector('#author').value = '';
+      const title = document.querySelector('#title').value;
+      const author = document.querySelector('#author').value;
+      if (title !== '' && author !== '') {
+        const book = new Book(title, author);
+        this.list.push(book);
+        localStorage.setItem('Books', JSON.stringify(this.list));
+        this.list = JSON.parse(localStorage.getItem('Books'));
+        this.render();
+      }
+      document.querySelector('#title').value = '';
+      document.querySelector('#author').value = '';
     }
 
     render() {
-        if (JSON.parse(localStorage.getItem('Books')) != null) {
-            this.list = JSON.parse(localStorage.getItem('Books'));
-            document.getElementById('showBooks').innerHTML = '';
-            this.list.forEach((book, index) => {
-                document.getElementById('showBooks').innerHTML += `
+      if (JSON.parse(localStorage.getItem('Books')) != null) {
+        this.list = JSON.parse(localStorage.getItem('Books'));
+        document.getElementById('showBooks').innerHTML = '';
+        this.list.forEach((book, index) => {
+          document.getElementById('showBooks').innerHTML += `
                 <div class="books">
                     <div class="d-flex">
                         <p class="title"><b>"${book.title}"  by ${book.author}</b></p>
                         <button class="removeBtn" onClick = 'books.remove(${index})'><b>Remove</b></button>
                     </div>
                     <hr>
-                </div> `
-            });
-        } else {
-            document.getElementById('showBooks').innerHTML = '';
-        }
+                </div> `;
+        });
+      } else {
+        document.getElementById('showBooks').innerHTML = '';
+      }
     }
+
     remove(index) {
-        this.list.splice(index, 1);
-        localStorage.setItem('Books', JSON.stringify(this.list));
-        this.render();
-        console.log(remove)
+      this.list.splice(index, 1);
+      localStorage.setItem('Books', JSON.stringify(this.list));
+      this.render();
+      console.log(remove);
     }
 }
 
-const books = new bookList();
+const books = new BookList();
 books.render();
 document.getElementById('addBtn').addEventListener('click', () => {
-    books.addBook();
-})
+  books.addBook();
+});
